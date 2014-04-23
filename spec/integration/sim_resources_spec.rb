7a5748c2f3404b0cb8b16ee9187064d9d2ebf90f
Chris Beer <cabeer@stanford.edu>
2014-04-23 15:43:17 -0700

    add simulated resourcelist integration test

---
+++ spec/integration/sim_resources_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "sim", vcr: true do
+  it "should validate a sitemap index" do
+    visit "/"
+    fill_in "sitemap_url", with: "http://resync.library.cornell.edu/sim100/resourcelist.xml"
+    click_on "Validate"
+    expect(page).to have_content "HTTP Status OK"
+  end
+end
